a73fceac276351afdcc981beef691874763a8842
shulga-eu <eugene.shulga@e-u.edu.ua>
2025-03-12 17:53:07 +0200

    hackerrank 4

---
+++ src/hackerrank/task04.rs
@@ -0,0 +1,12 @@
+fn diagonalDifference(arr: &[Vec<i32>]) -> i32 {
+    let n = arr.len();
+    let mut primary_diagonal_sum = 0;
+    let mut secondary_diagonal_sum = 0;
+
+    for i in 0..n {
+        primary_diagonal_sum += arr[i][i];
+        secondary_diagonal_sum += arr[i][n - i - 1];
+    }
+
+    (primary_diagonal_sum - secondary_diagonal_sum).abs()
+}
